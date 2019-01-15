a3fcaf2daeed76b931ce191e2645704b85262e20
Noah Biederbeck <noah.biederbeck@udo.edu>
2019-01-15 14:45:59 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="timing_magic_fact",
+    author="Noah Biederbeck",
+    version="0.1",
+    description="Compare timing information from the IACTs MAGIC and FACT",
+    packages=find_packages(),
+)
