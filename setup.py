9ac50c245ce14f2bebb324abd1f487fd86656dad
Noah Biederbeck <noah.biederbeck@udo.edu>
2019-01-15 14:54:47 +0100

    add required packages, fix #1

--- setup.py
+++ setup.py
@@ -3,7 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name="timing_magic_fact",
     author="Noah Biederbeck",
+    author_email="noah.biederbeck@tu-dortmund.de",
     version="0.1",
     description="Compare timing information from the IACTs MAGIC and FACT",
     packages=find_packages(),
+    install_requires=["requests", "bs4", "tqdm", "glob"],
 )
