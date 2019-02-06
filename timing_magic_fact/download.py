e5382f46f0a044df5b08c05f397381eddcf8b2c3
Noah Biederbeck <noah.biederbeck@udo.edu>
2019-02-06 16:03:47 +0100

    reformat and discard user

--- timing_magic_fact/download.py
+++ timing_magic_fact/download.py
@@ -3,14 +3,14 @@ import requests
 import bs4
 from tqdm import tqdm
 from os import path, makedirs
-from glob import glob
+
 
 class MAGICDownloader:
-    def __init__(self, password, user="MAGIC"):
-        self.password = password
+    def __init__(self, user, password):
         self.user = user
+        self.password = password
 
-    def download_files(self, urls: list, output_directory: str=".", chunk_size=1024):
+    def download_files(self, urls: list, output_directory: str = ".", chunk_size=1024):
         makedirs(output_directory, exist_ok=True)
         for url in urls:
             page = requests.get(url, auth=(self.user, self.password))
@@ -32,12 +32,14 @@ class MAGICDownloader:
                     continue
                 download_url = url + filename
                 print(f"Getting {download_url}")
-                stream = requests.get(download_url, stream=True, auth=(self.user, self.password))
+                stream = requests.get(
+                    download_url, stream=True, auth=(self.user, self.password)
+                )
                 file_size = stream.headers["content-length"]
-                with open(local_filename, 'wb') as f:
+                with open(local_filename, "wb") as f:
                     for chunk in tqdm(
                         stream.iter_content(chunk_size=chunk_size),
-                        total=int(int(file_size) / chunk_size)
+                        total=int(int(file_size) / chunk_size),
                     ):
-                        if chunk: # filter out keep-alive new chunks
+                        if chunk:  # filter out keep-alive new chunks
                             f.write(chunk)
