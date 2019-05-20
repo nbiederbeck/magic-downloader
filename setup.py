9a24dca2868c4c776477d4a37b01cd7abf539a38
Noah Biederbeck <noah.biederbeck@udo.edu>
2019-05-20 16:26:35 +0200

    Add long description and license

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as f:
+    long_description = f.read()
+
 setup(
     name="MAGICDownloader",
     author="Noah Biederbeck",
     author_email="noah.biederbeck@tu-dortmund.de",
-    version="0.1.0",
+    version="0.1.1",
     description="Download MAGIC data",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=find_packages(),
     install_requires=["requests", "bs4", "tqdm", "lxml"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
 )
